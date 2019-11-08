defbd839ef34ad22f2a91e9bccc7b470c9c2f6b3
EC2 Default User <ec2-user@ip-172-31-24-206.ec2.internal>
2019-11-08 21:18:40 +0000

    Organizing repo

--- cdk/cdk/ddbmodels_stack.py
+++ cdk/cdk/ddbmodels_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
-    core, aws_dynamodb, aws_lambda, aws_ec2, aws_ecs,
-    aws_apigateway, aws_iam, aws_s3, aws_ecr, aws_ssm, aws_codebuild
+    core, aws_dynamodb, aws_lambda,
+    aws_iam, aws_s3, aws_kinesis,
+    
 )
 from aws_cdk.aws_ec2 import SubnetType, Vpc
 from aws_cdk.core import App, Construct, Duration
@@ -18,6 +19,38 @@ class DynamoDBModelsStack(core.Stack):
             #read_capacity=3,
             #write_capacity=3)
             billing_mode=aws_dynamodb.BillingMode.PAY_PER_REQUEST)
+        
+        kds_input_stream = aws_kinesis.Stream(self, "dash_input_stream",
+            shard_count=1, 
+            stream_name="dashboard_input_stream")
+        
+        # Creating a ingest bucket for this stack
+        ingest_bucket = aws_s3.Bucket(self,'test-ngest-bucket')
+
+        #kda_agg_stream = 
+        
+        #Creating destination config
+        S3_dest_config = aws_kinesisfirehose.CfnDeliveryStreamProps.s3_destination_configuration(
+            BucketARN =ingest_bucket.bucket_arn,
+            BufferingHints = aws_kinesisfirehose.CfnDeliveryStream.BufferingHintsProperty(IntervalInSeconds= 60,
+            SizeInMBs= 5,
+            'CompressionFormat'= "UNCOMPRESSED",
+            "RoleARN" = " ")
+
+        #Creating firehose for this stack
+        firehose_test = aws_kinesisfirehose.CfnDeliveryStream(self, "testr",
+            s3_destination_configuration=S3_dest_config)
+        
+        #kfh_history_stream =
+        
+        lambda_agg_function = aws_lambda.Function(self, "AggDataLambda",
+            runtime=aws_lambda.Runtime.PYTHON_3_7,
+            handler="lambda_function.lambda_handler",
+            code=aws_lambda.Code.asset("../models/dashboard/aggregate_data_lambda"),
+            timeout=Duration.minutes(5))
+
+        lambda_agg_function.add_environment("BUCKET_NAME", "dreis-sandbox-temp")
+        lambda_agg_function.add_environment("DDB_TABLE_DEVICE_CATALOG", table.table_name)
         
         core.CfnOutput(
             self, "TableName_Dashboard",
--- models/checking_account/account_model.py
+++ models/checking_account/account_model.py
@@ -4,18 +4,13 @@ import os
 import random
 import json
 import datetime
-import time
 import decimal
 from string import ascii_letters
 import uuid
-from concurrent.futures import ThreadPoolExecutor
 
 LOG = logging.getLogger()
 LOG.setLevel(logging.INFO)
 TABLE_NAME = os.getenv('TABLE_NAME', 'DEFAULT_NAME')
-
-COMPANY = ["hotmail.com", "gmail.com", "aol.com", "mail.com" , "mail.kz", "yahoo.com"]
-letters = ascii_letters[:12]
 
 # Helper class to convert a DynamoDB item to JSON.
 class DecimalEncoder(json.JSONEncoder):
@@ -28,7 +23,7 @@ class DecimalEncoder(json.JSONEncoder):
         return super(DecimalEncoder, self).default(o)
 
 
-class DashboardModel:
+class AccountModel:
 
     _ddb_cli = None
     _ddb_res = None
@@ -44,7 +39,7 @@ class DashboardModel:
         self._ddb_res = boto3.resource('dynamodb')
         self._table = self._ddb_res.Table(table_name)
 
-
+    
     def deposit_money(self, acc_id, amount):
         print(">deposit_money ACC: '{}' AMOUNT: '{}'".format(acc_id, amount))
 
@@ -53,18 +48,18 @@ class DashboardModel:
             return
 
         now = datetime.datetime.now().isoformat()
-        tx_id = str(uuid.uuid4())
+        tx_id = str(uuid.uuid4())   
         type = "MoneyDeposit"
 
         r = self._ddb_cli.transact_write_items(
             TransactItems=[
-                {
+                {    
                     'Put': {
                         'TableName': TABLE_NAME,
                         'Item': {
                             "id": { "S": acc_id },
                             "sort": { "S" : "TX_{}".format(now)},
-                            "Amount": { "N": str(amount) },
+                            "Amount": { "N": str(amount) }, 
                             "SrcAccountId": { "S": "External" },
                             "DstAccountId": { "S": acc_id },
                             "CreditDebitIndicator": { "S": "Credit"},
@@ -75,7 +70,7 @@ class DashboardModel:
                         'ReturnValuesOnConditionCheckFailure': 'NONE'
                     }
                 },
-                {
+                {    
                     'Update': {
                         'TableName': TABLE_NAME,
                         'Key': { 'id': { "S": acc_id }, 'sort': { "S": "BALANCE"} },
@@ -85,9 +80,9 @@ class DashboardModel:
                             '#amount': 'Amount'
                         },
                         'ExpressionAttributeValues': {
-                            ':tx_amount': { "N": str(amount) }
+                            ':tx_amount': { "N": str(amount) } 
                         },
-                        'ReturnValuesOnConditionCheckFailure': 'NONE'
+                        'ReturnValuesOnConditionCheckFailure': 'NONE' 
                     }
                 }
             ],
@@ -106,18 +101,18 @@ class DashboardModel:
             return
 
         now = datetime.datetime.now().isoformat()
-        tx_id = str(uuid.uuid4())
+        tx_id = str(uuid.uuid4())   
         type = "MoneyWithdraw"
 
         r = self._ddb_cli.transact_write_items(
             TransactItems=[
-                {
+                {    
                     'Put': {
                         'TableName': TABLE_NAME,
                         'Item': {
                             "id": { "S": acc_id },
-                            "sort": { "S" : "TX_{}".format(now)},
-                            "Amount": { "N": str(amount) },
+                            "sort": { "S" : "TX#{}#{}".format(now, random.randint(0,1000)) },
+                            "Amount": { "N": str(amount) }, 
                             "SrcAccountId": { "S": acc_id },
                             "DstAccountId": { "S": "External" },
                             "CreditDebitIndicator": { "S": "Debit"},
@@ -128,7 +123,7 @@ class DashboardModel:
                         'ReturnValuesOnConditionCheckFailure': 'NONE'
                     }
                 },
-                {
+                {    
                     'Update': {
                         'TableName': TABLE_NAME,
                         'Key': { 'id': { "S": acc_id }, 'sort': { "S": "BALANCE"} },
@@ -138,9 +133,9 @@ class DashboardModel:
                             '#amount': 'Amount'
                         },
                         'ExpressionAttributeValues': {
-                            ':tx_amount': { "N": str(amount) }
+                            ':tx_amount': { "N": str(amount) } 
                         },
-                        'ReturnValuesOnConditionCheckFailure': 'NONE'
+                        'ReturnValuesOnConditionCheckFailure': 'NONE' 
                     }
                 }
             ],
@@ -159,11 +154,11 @@ class DashboardModel:
         print(">transfer_money FROM: {} TO: {} AMOUNT: {}".format(src_acc_id, dst_acc_id, tx_amount))
 
         now = datetime.datetime.now().isoformat()
-        tx_id = str(uuid.uuid4())
+        tx_id = str(uuid.uuid4())   
         type = "MoneyTransfer"
 
         tx_resp = {
-            "Amount": tx_amount,
+            "Amount": tx_amount, 
             "SrcAccountId": src_acc_id,
             "DstAccountId": dst_acc_id,
             "TransactionId": str(tx_id),
@@ -188,13 +183,13 @@ class DashboardModel:
         try:
             r = self._ddb_cli.transact_write_items(
                 TransactItems=[
-                    {
+                    {    
                         'Put': {
                             'TableName': TABLE_NAME,
                             'Item': {
                                 "id": { "S": src_acc_id },
                                 "sort": { "S" : "TX_{}".format(now)},
-                                "Amount": { "N": str(tx_amount) },
+                                "Amount": { "N": str(tx_amount) }, 
                                 "SrcAccountId": { "S": src_acc_id },
                                 "DstAccountId": { "S": dst_acc_id },
                                 "CreditDebitIndicator": { "S": "Debit"},
@@ -210,8 +205,8 @@ class DashboardModel:
                             'TableName': TABLE_NAME,
                             'Item': {
                                 "id": { "S": dst_acc_id },
-                                "sort": { "S" : "TX_{}".format(now)},
-                                "Amount": { "N": str(tx_amount) },
+                                "sort": { "S" : "TX#{}#{}".format(now, random.randint(1,1000))},
+                                "Amount": { "N": str(tx_amount) }, 
                                 "SrcAccountId": { "S": src_acc_id },
                                 "DstAccountId": { "S": dst_acc_id },
                                 "CreditDebitIndicator": { "S": "Credit"},
@@ -222,7 +217,7 @@ class DashboardModel:
                             'ReturnValuesOnConditionCheckFailure': 'NONE'
                         }
                     },
-                    {
+                    {    
                         'Update': {
                             'TableName': TABLE_NAME,
                             'Key': { 'id': { "S": src_acc_id }, 'sort': { "S": "BALANCE"} },
@@ -232,12 +227,12 @@ class DashboardModel:
                                 '#amount': 'Amount'
                             },
                             'ExpressionAttributeValues': {
-                                ':tx_amount': { "N": str(tx_amount) }
+                                ':tx_amount': { "N": str(tx_amount) } 
                             },
-                            'ReturnValuesOnConditionCheckFailure': 'NONE'
+                            'ReturnValuesOnConditionCheckFailure': 'NONE' 
                         }
                     },
-                    {
+                    {    
                         'Update': {
                             'TableName': TABLE_NAME,
                             'Key': { 'id': { "S": dst_acc_id }, 'sort': { "S": "BALANCE"} },
@@ -247,28 +242,28 @@ class DashboardModel:
                                 '#amount': 'Amount'
                             },
                             'ExpressionAttributeValues': {
-                                ':tx_amount': { "N": str(tx_amount) }
+                                ':tx_amount': { "N": str(tx_amount) } 
                             },
-                            'ReturnValuesOnConditionCheckFailure': 'NONE'
+                            'ReturnValuesOnConditionCheckFailure': 'NONE' 
                         }
                     }
                 ],
                 ReturnConsumedCapacity="TOTAL",
                 ReturnItemCollectionMetrics="SIZE"
             )
-
+        
             if r["ResponseMetadata"]["HTTPStatusCode"] == 200:
                 tx_resp["Result"] = True
             else:
                 print("####\n{}".format(json.dumps(r, indent=2, cls=DecimalEncoder)))
                 tx_resp["ErrorMsg"] = r
-
+        
         except Exception as e:
             print(e)
             tx_resp["ErrorMsg"] = str(e)
 
         return tx_resp
-
+        
 
     def query_statement(self, acc_id):
         print(">query_statement")
@@ -282,7 +277,7 @@ class DashboardModel:
             ScanIndexForward=True,
             ReturnConsumedCapacity="TOTAL"
         )
-
+        
         print(q["ConsumedCapacity"])
 
         return q
@@ -299,7 +294,7 @@ class DashboardModel:
             ScanIndexForward=True
         )
 
-        response = {
+        response = { 
             "AccountId": acc_id,
             "Balance": 0,
             "CreditDebitIndicator": "Credit",
@@ -313,274 +308,27 @@ class DashboardModel:
         else:
             print("No account data found!")
             response["ErrorMsg"] = "No account data found!"
-
+            
         return response
 
-    def query_company_by_minute(self, company_id):
-        q = self._table.query(
-            KeyConditionExpression="Pk = :pk AND begins_with(Sk, :sk)",
-            ExpressionAttributeValues={
-                ":pk": "{}".format(company_id),
-                ":sk": "W10MIN#"
-            },
-            ConsistentRead=False,
-            ScanIndexForward=True,
-            ReturnConsumedCapacity="TOTAL"
-        )
 
-        print(q["ConsumedCapacity"])
+    def create_new_account(self, email, init_balance):
+        print(">create_new_account")
 
-        return q
-
-
-    def query_company_by_minute_range(self, company_id, init, end):
-        q = self._table.query(
-            KeyConditionExpression="Pk = :pk AND Sk BETWEEN :init AND :end",
-            ExpressionAttributeValues={
-                ":pk": "{}".format(company_id),
-                ":init": "W10MIN#{}".format(init),
-                ":end": "W10MIN#{}".format(end)
-            },
-            ConsistentRead=False,
-            ScanIndexForward=True,
-            ReturnConsumedCapacity="TOTAL"
-        )
-
-        print(q["ConsumedCapacity"])
-
-        return q
-
-
-    def query_company_by_day(self, company_id):
-        q = self._table.query(
-            KeyConditionExpression="Pk = :pk AND begins_with(Sk, :sk)",
-            ExpressionAttributeValues={
-                ":pk": "{}".format(company_id),
-                ":sk": "DAY#"
-            },
-            ConsistentRead=False,
-            ScanIndexForward=True,
-            ReturnConsumedCapacity="TOTAL"
-        )
-
-        print(q["ConsumedCapacity"])
-
-        return q
-
-    def query_company_by_month(self, company_id):
-        q = self._table.query(
-            KeyConditionExpression="Pk = :pk AND begins_with(Sk, :sk)",
-            ExpressionAttributeValues={
-                ":pk": "{}".format(company_id),
-                ":sk": "MONTH#"
-            },
-            ConsistentRead=False,
-            ScanIndexForward=True,
-            ReturnConsumedCapacity="TOTAL"
-        )
-
-        print(q["ConsumedCapacity"])
-
-        return q
-
-    def insert_new_month_record(self, data):
-        logging.debug(">insert_new_month_record")
+        # TODO: Check if account exists
 
         self._table.put_item(Item={
-            "Pk": data["company_id"],
-            "Sk": "MONTH#{}".format(data["date"]),
-            "FatLiq": data["fat_liquido"],
-            "QtdVendas": data["qtd_vendas"],
-            "TicketMedio": data["tkt_medio"],
-            "FatBruto": data["fat_bruto"],
-            "Desconto": data["desconto"],
-            "Itens": data["itens"],
-            "QtdAtendimentos": data["qtd_atendimentos"],
-            "ValorTotal": data["valor_total"]
+            "id": email,
+            "sort": "CUSTOMER_DATA",
+            "Name": "Customer Name",
+            "Birth": "15-11-1900",
+            "Phone": "+551199999999",
+            "OverdraftAllowed": False
         })
-
-
-    def insert_new_minute_record(self, data):
-        logging.debug(">insert_new_minute_record")
-
+        
         self._table.put_item(Item={
-            "Pk": data["company_id"],
-            "Sk": "W10MIN#{}".format(data["date"]),
-            "FatLiq": data["fat_liquido"],
-            "QtdVendas": data["qtd_vendas"],
-            "TicketMedio": data["tkt_medio"],
-            "FatBruto": data["fat_bruto"],
-            "Desconto": data["desconto"],
-            "Itens": data["itens"],
-            "QtdAtendimentos": data["qtd_atendimentos"],
-            "ValorTotal": data["valor_total"]
+            "id": email,
+            "sort": "BALANCE",
+            "Amount": init_balance,
+            "CreditDebitIndicator": "Credit"
         })
-
-    def insert_new_day_record(self, data):
-        logging.debug(">insert_new_day_record")
-
-        self._table.put_item(Item={
-            "Pk": data["company_id"],
-            "Sk": "DAY#{}".format(data["date"]),
-            "FatLiq": data["fat_liquido"],
-            "QtdVendas": data["qtd_vendas"],
-            "TicketMedio": data["tkt_medio"],
-            "FatBruto": data["fat_bruto"],
-            "Desconto": data["desconto"],
-            "Itens": data["itens"],
-            "QtdAtendimentos": data["qtd_atendimentos"],
-            "ValorTotal": data["valor_total"]
-        })
-
-#####
-
-def random_timestamp(start, end):
-    frmt = '%Y-%m-%d %H:%M:%S'
-
-    stime = time.mktime(time.strptime(start, frmt))
-    etime = time.mktime(time.strptime(end, frmt))
-
-    ptime = stime + random.random() * (etime - stime)
-    dt = datetime.datetime.fromtimestamp(time.mktime(time.localtime(ptime)))
-    
-    return dt
-
-
-def random_date():
-    start_dt = datetime.date.today().replace(day=1, month=1).toordinal()
-    end_dt = datetime.date.today().toordinal()
-    random_day = datetime.date.fromordinal(random.randint(start_dt, end_dt))
-
-    return random_day
-
-
-def random_times():
-    return random_timestamp("2015-01-01 13:30:00", "2019-11-01 04:50:34")
-
-
-def random_month_formatted():
-    return random_date().strftime("%Y-%m")
-
-
-def random_date_formatted():
-    return random_date().strftime("%Y-%m-%d")
-
-
-def random_hour_minute_formatted():
-    return random_times().strftime("%Y-%m-%d_%H:%M")
-
-
-def generate_random_data(period="D"):
-    data = {}
-
-    if period == "D":
-        data["date"] = random_date_formatted()
-    elif period == "M":
-        data["date"] = random_month_formatted()
-    else:
-        data["date"] = random_hour_minute_formatted()
-
-    data["company_id"] = random.choice(COMPANY)
-    data["fat_liquido"] = random.randint(0, 1000)
-    data["qtd_vendas"] = random.randint(0, 100)
-    data["tkt_medio"] = random.randint(1, 50)
-    data["fat_bruto"] = random.randint(1, 1000)
-    data["desconto"] = random.randint(0, 1000)
-    data["itens"] = random.randint(0, 1000)
-    data["qtd_atendimentos"] = random.randint(0, 1000)
-    data["valor_total"] =random.randint(0, 1000)
-
-    return data
-
-def load_days(n):
-    logging.info(">load_data {}".format(n))
-
-    dm = DashboardModel(table_name=TABLE_NAME)
-    
-    for i in range(1, 500):
-        data = generate_random_data()
-        dm.insert_new_day_record(data)
-        #companies.add(data["company_id"])
-        
-        if i % 50 == 0:
-            logging.info("THREAD {} I {}".format(n, i))
-
-
-def load_hour_min(n):
-    logging.info(">load_data {}".format(n))
-
-    dm = DashboardModel(table_name=TABLE_NAME)
-    
-    for i in range(1, 500):
-        data = generate_random_data(period="mm")
-        dm.insert_new_minute_record(data)
-        #companies.add(data["company_id"])
-        
-        if i % 50 == 0:
-            logging.info("THREAD {} I {}".format(n, i))
-
-
-def load_hour_min_in_parallel(n):
-    return
-
-if __name__ == "__main__":
-    dm = DashboardModel(table_name=TABLE_NAME)
-    
-    print(random_hour_minute_formatted())
-
-    companies = set()
-
-    # with ThreadPoolExecutor(max_workers=8) as executor:
-    #     future = executor.submit(load_days, (1))
-    #     future = executor.submit(load_days, (2))
-    #     future = executor.submit(load_days, (3))
-    #     future = executor.submit(load_days, (4))
-    #     future = executor.submit(load_days, (5))
-    #     future = executor.submit(load_days, (6))
-    #     future = executor.submit(load_days, (7))
-    #     future = executor.submit(load_days, (8))
-
-    # with ThreadPoolExecutor(max_workers=8) as executor:
-    #     future = executor.submit(load_hour_min, (1))
-    #     future = executor.submit(load_hour_min, (2))
-    #     future = executor.submit(load_hour_min, (3))
-    #     future = executor.submit(load_hour_min, (4))
-    #     future = executor.submit(load_hour_min, (5))
-    #     future = executor.submit(load_hour_min, (6))
-    #     future = executor.submit(load_hour_min, (7))
-    #     future = executor.submit(load_hour_min, (8))
-
-
-    for cpy in COMPANY:
-        start = time.time()
-        
-        #q = dm.query_company_by_minute(cpy)
-        #q = dm.query_company_by_minute_range(cpy, "2015-11-08", "0", "24")
-        q = dm.query_company_by_day_range(cpy, "2015-11-01", "2019-11-07")
-
-        end = time.time()
-        print(end - start)
-        
-        tkt_medio = 0
-        fat_liquido = 0
-        qtd_vendas = 0
-        fat_bruto = 0
-        recs = 0
-
-        for items in q['Items']:
-            #print(items)
-            tkt_medio += items["TicketMedio"]
-            fat_liquido += items["FatLiq"]
-            qtd_vendas += items["QtdVendas"]
-            fat_bruto += items["FatBruto"]
-            recs += 1
-            #"Desconto": data["desconto"],
-            #"Itens": data["itens"],
-            #"QtdAtendimentos": data["qtd_atendimentos"],
-            #"ValorTotal": data["valor_total"]
-
-        #print(q)
-        end = time.time()
-        print(end - start)
-        print("\n####\nCOMPANY {}\nTKT_MEDIO {} FAT_LIQ {} QTD_VENDAS {}\nRECS {}".format(cpy, tkt_medio, fat_liquido, qtd_vendas, recs))
-
